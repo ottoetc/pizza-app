ff0e0d98b14740cd309d372b77adfe2f1039988e
Nathan Otto <ottoetc@gmail.com>
2016-01-29 11:44:47 -0800

    add jumbotron to html

--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,11 @@
 describe('Pizza', function() {
   it("creates a new object with the given properties", function() {
-    var testPizza = new Pizza("sm", (["mushrooms", "pepperoni", "cheese", "sauce"]));
-    expect(testPizza.pizzaSize).to.equal("sm");
+    var testPizza = new Pizza("Medium", (["mushrooms", "pepperoni", "cheese", "sauce"]));
+    expect(testPizza.pizzaSize).to.equal("Medium");
     expect(testPizza.pizzaToppings).to.eql(["mushrooms", "pepperoni", "cheese", "sauce"]);
   });
   it("calculates cost of the pizza using the pizzaCost method", function() {
-    var testPizza = new Pizza("sm", (["mushrooms", "pepperoni", "cheese", "sauce"]));
-    expect(testPizza.pizzaCost()).to.equal(12);
+    var testPizza = new Pizza("Large", (["mushrooms", "pepperoni", "cheese", "sauce"]));
+    expect(testPizza.pizzaCost()).to.equal(20);
   });
 });
