6b1cc4a2f99a80b7ce219d6ae96976cc2f4f5723
Nathan Otto <ottoetc@gmail.com>
2016-01-29 11:19:54 -0800

    add comments to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 var Pizza = function(pizzaSize, pizzaToppings) {
   this.pizzaSize = pizzaSize;
-  console.log("Size: ", this.pizzaSize);
   this.pizzaToppings = pizzaToppings;
-  console.log("Toppings: ", this.pizzaToppings);
 }
-
+// Calculates cost of Pizza starts with a base cost of 8 and modifies based on size and number of toppings
 Pizza.prototype.pizzaCost = function() {
   var cost = 8;
+  // Large Cost
   if(this.pizzaSize === "Large") {
     cost = cost * 2 + (this.pizzaToppings.length * 1);
-    console.log("Large Cost: ", cost);
-  } else if (this.pizzaSize === "Medium") {
+  }
+  // Medium Cost
+  else if (this.pizzaSize === "Medium") {
     cost = cost + 4 + (this.pizzaToppings.length * 1);
-    console.log("Medium Cost: ", cost);
-  } else {
+  }
+  // Small Cost
+  else {
     cost = cost + (this.pizzaToppings.length * 1);
-    console.log("Small Cost: ", cost);
   }
   return cost;
 }
@@ -23,18 +23,18 @@ Pizza.prototype.pizzaCost = function() {
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
+    // Gets Pizza size from select box
     var inputPizzaSize = $("select.size").val();
-    console.log("Pizza Size: ", inputPizzaSize);
-
+    // Gets Pizza toppings by pushing values of all checked topping boxes to an array
     var inputPizzaToppings = [];
-    $("input:checked").each(function() {
+    $("input:checked").each(function() { // Targets only checked inputs
       inputPizzaToppings.push($(this).val());
     });
-
-    console.log("Toppings Array: ", inputPizzaToppings);
+    // Creates Pizza object
     var pizza = new Pizza(inputPizzaSize, inputPizzaToppings);
-    console.log("Pizza: ", pizza);
+    console.log("Pizza Object: ", pizza); // This logs the size and toppings of the pizza object
 
+    // Outputs the price of the created pizza
     $("ul#pizzaList").append("<li><span class='pizza'>" + pizza.pizzaCost() + "</span></li>");
   });
 });
