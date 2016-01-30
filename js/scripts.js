11296ced80934d9e7a07df6a26ea537b537950f3
Nathan Otto <ottoetc@gmail.com>
2016-01-29 16:49:45 -0800

    add output display logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-var Pizza = function(pizzaSize, pizzaToppings) {
+var Pizza = function(pizzaSize, pizzaToppings, customerName) {
+  this.customerName = customerName;
   this.pizzaSize = pizzaSize;
   this.pizzaToppings = pizzaToppings;
+
 }
 // Calculates cost of Pizza starts with a base cost of 8 and modifies based on size and number of toppings
 Pizza.prototype.pizzaCost = function() {
-  var cost = 8;
   // Large Cost
+  var cost = 8;
   if(this.pizzaSize === "Large") {
     cost = cost * 2 + (this.pizzaToppings.length * 1);
   }
@@ -24,17 +26,32 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     // Gets Pizza size from select box
+    var inputCustomerName = $("input#name").val();
     var inputPizzaSize = $("select.size").val();
     // Gets Pizza toppings by pushing values of all checked topping boxes to an array
     var inputPizzaToppings = [];
     $("input:checked").each(function() { // Targets only checked inputs
       inputPizzaToppings.push($(this).val());
     });
+
     // Creates Pizza object
-    var pizza = new Pizza(inputPizzaSize, inputPizzaToppings);
+    var pizza = new Pizza(inputPizzaSize, inputPizzaToppings, inputCustomerName);
     console.log("Pizza Object: ", pizza); // This logs the size and toppings of the pizza object
 
     // Outputs the price of the created pizza
-    $("ul#pizzaList").append("<li><span class='pizza'>" + pizza.pizzaCost() + "</span></li>");
+    $("ul#pizzaList").append("<li><p class='pizza-link'>" + pizza.customerName + "'s Pizza</p></li>");
+    // $("ul#pizzaList").append("<li><span class='pizza'>" + pizza.pizzaCost() + "</span></li>");
+    $(".pizza-link").last().click(function() {
+      $("#show-order").show();
+      $(".customer-name").text(pizza.customerName);
+      $(".pizza-size").text(pizza.pizzaSize);
+      $("ul#pizza-toppings").text("");
+      pizza.pizzaToppings.forEach(function(pizzaTopping) {
+        $("ul#pizza-toppings").append("<li>" + pizzaTopping + "</li>");
+      });
+      $(".pizza-cost").text(pizza.pizzaCost());
+
+
+    });
   });
 });
